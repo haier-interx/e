e91991072cd776dfb5750c450a9040cc5a3e57ef
xuebing <xuebing.it@haier.com>
2020-02-27 09:37:55 +0800

    U: use chinese message

--- auth.go
+++ auth.go
@@ -1,9 +1,9 @@
 package e
 
 var (
-	AUTH_NOTLOGIN                 = &Err{40001, "user not login"}
-	AUTH_NOPERMISSION             = &Err{40003, "user no permission"}
-	AUTHINTERNAL_ROLE_NOT_DEFINED = &Err{40004, "role not defined"}
-	AUTHINTERNAL_ROLE_NOT_SUPPORT = &Err{40005, "role not support"}
-	AUTHINTERNAL_ROLE_INVALID     = &Err{40006, "role invalid"}
+	AUTH_NOTLOGIN                 = &Err{40001, "您未登录或登录过期，请登录"}
+	AUTH_NOPERMISSION             = &Err{40003, "您无权访问"}
+	AUTHINTERNAL_ROLE_NOT_DEFINED = &Err{40004, "用户角色不可用，请联系系统管理员更正"}
+	AUTHINTERNAL_ROLE_NOT_SUPPORT = &Err{40005, "无此角色信息，请检查配置权限是否正确"}
+	AUTHINTERNAL_ROLE_INVALID     = &Err{40006, "用户角色不可用"}
 )
--- common.go
+++ common.go
@@ -11,15 +11,39 @@ func (e *Err) Error() string {
 
 var (
 	COMMON_SUC        = &Err{10000, "success"}
-	COMMON_BADREQUEST = &Err{10001, "bad request"}
-	COMMON_PARAM_ERR  = &Err{10002, "request parameter error"}
-	COMMON_PARAM_MISS = &Err{10003, "request parameter missing"}
-	COMMON_NOT_FOUND  = &Err{10004, "not found"}
-	COMMON_CONFILCT   = &Err{10005, "conflict"}
+	COMMON_BADREQUEST = &Err{10001, "请求错误，请更正后重试"}
+	COMMON_PARAM_ERR  = &Err{10002, "请求参数错误，请更正后重试"}
+	COMMON_PARAM_MISS = &Err{10003, "请求参数缺失，请补充后重试"}
+	COMMON_NOT_FOUND  = &Err{10004, "未找到请求资源，请稍后重试"}
+	COMMON_CONFILCT   = &Err{10005, "请求冲突，请稍后重试"}
 
-	COMMON_INTERNAL_ERR             = &Err{15000, "unknown internal error"}
-	COMMON_INTERNAL_CALLING_ERR     = &Err{15001, "internal calling error"}
-	COMMON_INTERNAL_CALLING_TIMEOUT = &Err{15002, "internal calling timeout"}
-	COMMON_INTERNAL_DB_ERR          = &Err{15003, "internal database error"}
-	COMMON_INTERNAL_BADGATEWAY      = &Err{15004, "bad gateway"}
+	COMMON_INTERNAL_ERR             = &Err{15000, "内部错误，请稍后重试"}
+	COMMON_INTERNAL_CALLING_ERR     = &Err{15001, "内部调用错误，请稍后重试"}
+	COMMON_INTERNAL_CALLING_TIMEOUT = &Err{15002, "内部调用无响应，请稍后重试"}
+	COMMON_INTERNAL_DB_ERR          = &Err{15003, "内部调用数据库错误，请稍后重试"}
+	COMMON_INTERNAL_BADGATEWAY      = &Err{15004, "网关错误，请稍后重试"}
 )
+
+func NewBadRequestError(msg string) *Err {
+	return &Err{COMMON_BADREQUEST.Code, msg}
+}
+
+func NewInternalError(msg string) *Err {
+	return &Err{COMMON_INTERNAL_ERR.Code, msg}
+}
+
+func NewParamError(msg string) *Err {
+	return &Err{COMMON_PARAM_ERR.Code, msg}
+}
+
+func NewParamMissError(msg string) *Err {
+	return &Err{COMMON_PARAM_MISS.Code, msg}
+}
+
+func NewInternalDBError(msg string) *Err {
+	return &Err{COMMON_INTERNAL_DB_ERR.Code, msg}
+}
+
+func NewInternalCallTimeoutError(msg string) *Err {
+	return &Err{COMMON_INTERNAL_CALLING_TIMEOUT.Code, msg}
+}
